dba9b2609bf5f81fc1e6c0e48c33e5d0029a9653
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-09-10 21:52:55 -0400

    completed question 4

--- sorting-drills.js
+++ sorting-drills.js
@@ -28,14 +28,14 @@ function createArr (){
 }
 
 
-function quickSort (array, start= 0, end = array.length){
+function qSort(array, start= 0, end = array.length){
 
   if(start >= end){
     return array;
   }
   const middle = partition(array, start, end);
-  array = quickSort(array, start, middle);
-  array = quickSort(array, middle + 1, end);
+  array = qSort(array, start, middle);
+  array = qSort(array, middle + 1, end);
   return array;
 }
 function partition(array, start, end){
@@ -58,7 +58,44 @@ function swap(array, i, j){
 }
 
 let testArr = createArr();
-console.log(quickSort(testArr));
+// console.log(qSort(testArr));
+
+
+function mSort(array){
+  if(array.length <= 1){
+    return array;
+  }
+  const middle = Math.floor(array.length/2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
+
+  left = mSort(left);
+  right = mSort(right);
+  return merge(left, right, array);
+}
+
+function merge(left, right, array){
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+  while(leftIndex < left.length && rightIndex < right.length) {
+    if(left[leftIndex] < right[rightIndex]){
+      array[outputIndex++] = left[leftIndex++]
+    }
+    else{
+      array[outputIndex++] = right[rightIndex++]
+    }
+  }
+  for (let i = leftIndex; i < left.length; i++){
+    array[outputIndex++] = left[i];
+  }
+  for (let i = rightIndex; i < right.length; i++){
+    array[outputIndex++] = right[i];
+  }
+  return array;
+}
+
+console.log(mSort(testArr))
 
 
 // 4. Implementing merge sort
